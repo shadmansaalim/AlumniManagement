84c63596637c3d98e91c6af11c5015399e2691cc
shadmansaalim321 <shadmansaalim321@gmail.com>
2023-05-04 21:45:17 +0600

    React router dom added for routing between routes

--- frontend-alumni-management/src/App.jsx
+++ frontend-alumni-management/src/App.jsx
@@ -6,10 +6,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 
+// React Router DOM
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <RouterProvider router={router} />
     </div>
   )
 }
