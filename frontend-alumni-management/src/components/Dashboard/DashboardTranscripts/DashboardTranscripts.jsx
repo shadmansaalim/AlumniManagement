ead1a75e26d3fd6c6d1d858137b5bc0645e1e085
shadmansaalim321 <shadmansaalim321@gmail.com>
2023-05-10 12:53:34 +0600

    Dashboard transcripts tab, graduation certificate created using REACT-PDF

--- frontend-alumni-management/src/components/Dashboard/DashboardTranscripts/DashboardTranscripts.jsx
+++ frontend-alumni-management/src/components/Dashboard/DashboardTranscripts/DashboardTranscripts.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import { Document, Page, StyleSheet, Font, Text, View, Image } from '@react-pdf/renderer';
 import { PDFViewer } from '@react-pdf/renderer';
 
+import useAuth from '../../../hooks/useAuth';
+
+// Register EB Garamond font
+Font.register({
+    family: 'Ubuntu',
+    fonts: [
+        {
+            src: 'https://fonts.gstatic.com/s/questrial/v13/QdVUSTchPBm7nuUeVf7EuStkm20oJA.ttf',
+        },
+        {
+            src: 'https://fonts.gstatic.com/s/questrial/v13/QdVUSTchPBm7nuUeVf7EuStkm20oJA.ttf',
+            fontWeight: 'bold',
+        },
+        {
+            src: 'https://fonts.gstatic.com/s/questrial/v13/QdVUSTchPBm7nuUeVf7EuStkm20oJA.ttf',
+            fontWeight: 'normal',
+            fontStyle: 'italic',
+        },
+    ],
+});
 
 // Create styles
 const styles = StyleSheet.create({
@@ -10,6 +30,10 @@ const styles = StyleSheet.create({
         padding: '50px',
         fontSize: '18px',
         lineHeight: '1.5',
+        fontFamily: 'Ubuntu',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
     },
     title: {
         textAlign: 'center',
@@ -24,11 +48,11 @@ const styles = StyleSheet.create({
     text: {
         textAlign: 'justify',
         marginBottom: '20px',
-        fontStyle: 'italic'
+        fontStyle: 'italic',
     },
     signature: {
         marginTop: '20px',
-        textAlign: 'center',
+        textAlign: 'start',
         fontWeight: 'bold',
         textTransform: 'uppercase',
     },
@@ -39,14 +63,11 @@ const styles = StyleSheet.create({
     signatoryTitle: {
         fontWeight: 'normal',
     },
-    separator: {
-        margin: '50px 0',
-    },
     transcript: {
-        textAlign: 'center',
+        textAlign: 'start',
         fontSize: '24px',
         fontWeight: 'bold',
-        marginBottom: '24px',
+        marginBottom: '16px',
         marginTop: '16px',
         textTransform: 'uppercase',
         textDecoration: 'underline'
@@ -82,34 +103,39 @@ const styles = StyleSheet.create({
 });
 
 const transcript = [
-    { name: 'Mathematics', mark: 'A+' },
-    { name: 'Computer Science', mark: 'A+' },
-    { name: 'Statistics', mark: 'A' },
-    { name: 'Physics', mark: 'B+' },
+    { name: 'Data Structure & Algorithms', mark: '3.24' },
+    { name: 'Web/Mobile Programming', mark: '3.50' },
+    { name: 'Cloud Computing', mark: '4.00' },
+    { name: 'Database Management', mark: '3.80' },
 ];
 
 const DashboardTranscripts = () => {
+    const { user } = useAuth();
+    console.log(user);
     return (
         <PDFViewer style={{ width: '100%', height: '100vh' }}>
             <Document>
                 <Page size="A4" style={styles.page}>
-                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/RMIT_University_Logo.svg/2560px-RMIT_University_Logo.svg.png" style={styles.logo} />
-                    <Text style={styles.title}>Graduation Certificate</Text>
-                    <Text style={styles.text}>
-                        This is to certify that <Text style={{ fontWeight: 'bold' }}>John Doe</Text> has completed all the requirements
-                        for the degree of <Text style={{ fontWeight: 'bold' }}>Bachelor of Science in Computer Science</Text> at <Text style={{ fontWeight: 'bold' }}>RMIT University</Text>, Melbourne, Australia.{' '}
-                    </Text>
-                    <Text style={styles.transcript}>Transcript</Text>
-                    {transcript.map((course, index) => (
-                        <View key={index} style={styles.transcriptCourses}>
-                            <Text style={styles.transcriptCourseName}>{course.name}</Text>
-                            <Text style={styles.transcriptCourseMark}>{course.mark}</Text>
-                        </View>
-                    ))}
-                    <Text style={styles.signature}>Signature</Text>
-                    <View style={styles.separator} />
-                    <Text style={styles.signatory}>Dr. Jane Smith</Text>
-                    <Text style={styles.signatoryTitle}>Head of Department</Text>
+                    <View>
+                        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/RMIT_University_Logo.svg/2560px-RMIT_University_Logo.svg.png" style={styles.logo} />
+                        <Text style={styles.title}>Graduation Certificate</Text>
+                        <Text style={styles.text}>
+                            This is to certify that <Text style={{ fontWeight: 'bold' }}>{user.displayName}</Text> has completed all the requirements for the degree of Bachelor of Science in Computer Science and has achieved distinction this year 2023 at RMIT University, Melbourne, Australia.
+                        </Text>
+
+                        <Text style={styles.transcript}>Transcript</Text>
+                        {transcript.map((course, index) => (
+                            <View key={index} style={styles.transcriptCourses}>
+                                <Text style={styles.transcriptCourseName}>{course.name}</Text>
+                                <Text style={styles.transcriptCourseMark}>{course.mark}</Text>
+                            </View>
+                        ))}
+                    </View>
+                    <View>
+                        <Text style={styles.signature}>Jane</Text>
+                        <Text style={styles.signatory}>Dr. Jane Smith</Text>
+                        <Text style={styles.signatoryTitle}>Head of Department</Text>
+                    </View>
                 </Page>
             </Document>
         </PDFViewer>
