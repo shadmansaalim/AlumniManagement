dca76ddfab8e9b31cf4080200553aa54c691e1c0
“osith” <you@example.com>
2023-05-17 17:25:06 +1000

    Remove auth from create student

--- backend-alumni-management/app/modules/student/student.route.ts
+++ backend-alumni-management/app/modules/student/student.route.ts
@@ -7,7 +7,7 @@ import { authGuard } from '../auth-token/check-auth-user';
 
 // Root API
 router.get('/', authGuard, getStudents);
-router.post('/create-student', authGuard, createStudent);
+router.post('/create-student', createStudent);
 
 
 export default router;
